a9de83aa14a0669b1e99ff62cacb57904ff54c22
rysson <robert.kalinowski@sharkbits.com>
2022-02-24 14:45:56 +0100

    Move code to /main.py

--- main.py
+++ main.py
@@ -1,3 +1,44 @@
+#   GNU General Public License
+
+#   TVP GO KODI Addon
+#   Copyright (C) 2022 Kodi-PL
+#   Copyright (C) 2022 Mariusz89B
+#   Copyright (C) 2022 mtr81
+#   Copyright (C) 2022 m1992
+
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+
+#   You should have received a copy of the GNU General Public License
+#   along with this program. If not, see https://www.gnu.org/licenses.
+
+#   MIT License
+
+#   Permission is hereby granted, free of charge, to any person obtaining a copy
+#   of this software and associated documentation files (the "Software"), to deal
+#   in the Software without restriction, including without limitation the rights
+#   to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#   copies of the Software, and to permit persons to whom the Software is
+#   furnished to do so, subject to the following conditions:
+
+#   The above copyright notice and this permission notice shall be included in all
+#   copies or substantial portions of the Software.
+
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#   AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#   OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#   SOFTWARE.
+
 from six.moves import urllib_parse
 import xbmcgui
 import xbmcplugin
@@ -501,3 +542,9 @@ class Main(SimplePlugin):
         finally:
             f.close()
         xbmcgui.Dialog().notification('TVP GO', L(30029, 'Playlist M3U generated'), xbmcgui.NOTIFICATION_INFO)
+
+
+if __name__ == '__main__':
+    import sys
+    log.info(f'============= {sys.argv}')
+    Main().run()
